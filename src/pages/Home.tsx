ce95a707a9b82e08bf8cba476dd437d7ef18aed5
King143rahul <87023775+King143rahul@users.noreply.github.com>
2025-11-16 16:24:41 +0530

    Add ServicesSection to Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { Header } from "@/components/home/Header";
 import { HeroSection } from "@/components/home/HeroSection";
 import { PartnersSection } from "@/components/home/PartnersSection";
+import { ServicesSection } from "@/components/home/ServicesSection";
 import { BenefitsSection } from "@/components/home/BenefitsSection";
 import { LocationSection } from "@/components/home/LocationSection";
 import { Footer } from "@/components/home/Footer";
@@ -7,8 +9,10 @@ import { Footer } from "@/components/home/Footer";
 const Home = () => {
   return (
     <div className="min-h-screen bg-background">
+      <Header />
       <HeroSection />
       <PartnersSection />
+      <ServicesSection />
       <BenefitsSection />
       <LocationSection />
       <Footer />
