e657c3b3061d9d2f678d8a16bebeb1567b57e7be
King143rahul <87023775+King143rahul@users.noreply.github.com>
2025-11-16 16:37:23 +0530

    Update Header.tsx

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "./ModeToggle";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-6xl items-center px-4">
         <div className="mr-4 flex">
           <a href="/" className="flex items-center space-x-2">
